9762b38fbcc23c98f819a73843e65f773d2deeed
thisnameisprivate <2632798785@qq.com>
2018-12-17 17:20:00 +0800

    save code.

--- inputExe.py
+++ inputExe.py
@@ -3,14 +3,14 @@ from tkinter import *
 root = Tk()
 li = ['C', 'python', 'php', 'html', 'SQL', 'java']
 movie = ['CSS', 'jQuery', 'Bootstrap']
-listb = Listbox(root)  # 创建两个列表组件
+listb = Listbox(root)
 listb2 = Listbox(root)
-for item in li:  # 第一个小部件插入数据
+for item in li:
     listb.insert(0, item)
 
-for item in movie:  # 第二个小部件插入数据
+for item in movie:
     listb2.insert(0, item)
 
-listb.pack()  # 将小部件放置到主窗口中
+listb.pack()
 listb2.pack()
-root.mainloop()  # 进入消息循环
+root.mainloop()
